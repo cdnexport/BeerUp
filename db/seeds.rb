41ef313450823e8b789ceb5de00306c69cfac491
cdnexport <fudgemaster3000@hotmail.com>
2019-03-16 18:05:21 -0500

    store and hours seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Hour.destroy_all
+Store.destroy_all
+
+url = 'http://ontariobeerapi.ca/stores/'
+storesResponse = Net::HTTP.get(URI(url))
+storesJson = JSON.parse(storesResponse)
+
+storesJson.each do |store|
+  newStore = Store.create(
+    id: store["store_id"],
+    name: store["name"],
+    address: store["address"],
+    city: store["city"],
+    postal_code: store["postal_code"],
+    phone: store["phone"],
+    latitude: store["latitude"],
+    longitude: store["longitude"]
+  )
+  newStore.hours.create(
+    day: "Monday",
+    open: store["monday_open"],
+    close: store["monday_close"]
+  )
+  newStore.hours.create(
+    day: "Tuesday",
+    open: store["tuesday_open"],
+    close: store["tuesday_close"]
+  )
+  newStore.hours.create(
+    day: "Wednesday",
+    open: store["wednesday_open"],
+    close: store["wednesday_close"]
+  )
+  newStore.hours.create(
+    day: "Thursday",
+    open: store["thursday_open"],
+    close: store["thursday_close"]
+  )
+  newStore.hours.create(
+    day: "Friday",
+    open: store["friday_open"],
+    close: store["friday_close"]
+  )
+  newStore.hours.create(
+    day: "Saturday",
+    open: store["saturday_open"],
+    close: store["saturday_close"]
+  )
+  newStore.hours.create(
+    day: "Sunday",
+    open: store["sunday_open"],
+    close: store["sunday_close"]
+  )
+end
