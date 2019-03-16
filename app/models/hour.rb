aa698b8437866f9616844b3752b109e5d1104d33
cdnexport <fudgemaster3000@hotmail.com>
2019-03-16 18:03:44 -0500

    changed time regex

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -1,8 +1,9 @@
 class Hour < ApplicationRecord
   belongs_to :store
+
   validates :store, :day, :open, :close, presence: true
   validates :open, :close, format: {
-    with: /\d{2}(:\d{2})? [ap]m/,
+    with: /\d\d? [ap]m/,
     message: "time must be entered like 10 am"
   }
 end
