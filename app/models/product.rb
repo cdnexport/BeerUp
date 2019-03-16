fec3ffee6fb01d756686984ef81ee41e7ff9d110
cdnexport <fudgemaster3000@hotmail.com>
2019-03-16 16:45:20 -0500

    added category to validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  validates :name, :price, :description, :alcohol, :container, :country, :size, :manufacturer, :image, presence: true
+  validates :name, :price, :description, :alcohol, :container, :country, :size, :manufacturer, :image, :category, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :alcohol, numericality: { greater_than_or_equal_to: 0 }
 end
