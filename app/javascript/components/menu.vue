2bb127d13aef985027a1ec9f07dcbf2b214004a3
cdnexport <fudgemaster3000@hotmail.com>
2019-03-24 13:58:52 -0500

    fixed hamburger

--- app/javascript/components/menu.vue
+++ app/javascript/components/menu.vue
@@ -50,6 +50,19 @@
 import storeLocation from './storeLocation.vue';
 export default {
     components: {storeLocation},
+    mounted: function () {
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        if ($navbarBurgers.length > 0) {
+            $navbarBurgers.forEach( el => {
+                el.addEventListener('click', () => {
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+                });
+            });
+        }
+    }
 }
 </script>
 <style scoped>
