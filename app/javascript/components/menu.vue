b73f5c2b5170a0118886dda8f4975bd09f92d97a
cdnexport <fudgemaster3000@hotmail.com>
2019-03-25 15:47:59 -0500

    logo now showing

--- app/javascript/components/menu.vue
+++ app/javascript/components/menu.vue
@@ -3,7 +3,7 @@
         <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <a class="navbar-item" href="/">
-                <!-- <img src="../assets/images/beer.png" height="40"> -->
+                <img src="../images/beer.png" height="40" />
                 BeerUp
                 </a>
                 <storeLocation />
